e431b02675d5726211247d067f8d1638e80f3159
Srimurari14 <murari.dachepalli@gmail.com>
2026-01-21 17:40:32 -0500

    orchestration

--- scripts/fact_weather_current.py
+++ scripts/fact_weather_current.py
@@ -1,83 +1,78 @@
-import json
-import pandas as pd
-from configs.cities import CITIES
-from scripts.validations import validate_fact_weather_current
-from scripts.validations import validate_dim_location
+def run_fact_weather_current():
+    import json
+    import pandas as pd
+    from configs.cities import CITIES
+    from scripts.validations import validate_fact_weather_current, validate_dim_location
 
-all_current = []
+    all_current = []
 
-for city_cfg in CITIES:
-    with open(city_cfg["current_file"]) as f:
-        raw = json.load(f)
+    for city_cfg in CITIES:
+        with open(city_cfg["current_file"]) as f:
+            raw = json.load(f)
 
-    df = pd.json_normalize(raw)
+        df = pd.json_normalize(raw)
 
-    df['weather_description'] = df['weather'].apply(
-        lambda x: x[0]['description'] if isinstance(x, list) and len(x) > 0 else None
+        df['weather_description'] = df['weather'].apply(
+            lambda x: x[0]['description'] if isinstance(x, list) and len(x) > 0 else None
+        )
+
+        df.rename(columns={
+            'dt': 'date_time',
+            'name': 'city',
+            'coord.lon': 'longitude',
+            'coord.lat': 'latitude',
+            'main.temp': 'temperature',
+            'main.feels_like': 'feels_like',
+            'main.temp_min': 'temp_min',
+            'main.temp_max': 'temp_max',
+            'main.pressure': 'pressure',
+            'main.humidity': 'humidity',
+            'wind.speed': 'wind_speed',
+            'wind.deg': 'wind_direction',
+            'clouds.all': 'cloud_cover',
+            'sys.country': 'country'
+        }, inplace=True)
+
+        df['location_id'] = city_cfg["location_id"]
+        df['date_time'] = pd.to_datetime(df['date_time'], unit='s', utc=True)
+
+        df = df[
+            [
+                'location_id',
+                'city',
+                'longitude',
+                'latitude',
+                'country',
+                'date_time',
+                'temperature',
+                'feels_like',
+                'temp_min',
+                'temp_max',
+                'pressure',
+                'humidity',
+                'wind_speed',
+                'wind_direction',
+                'cloud_cover',
+                'weather_description'
+            ]
+        ]
+
+        all_current.append(df)
+
+    fact_weather_current = pd.concat(all_current, ignore_index=True)
+
+    dim_location = (
+        fact_weather_current[
+            ['location_id', 'city', 'country', 'latitude', 'longitude']
+        ]
+        .drop_duplicates()
+        .reset_index(drop=True)
     )
 
-    df.rename(columns={
-        'dt': 'date_time',
-        'name': 'city',
-        'coord.lon': 'longitude',
-        'coord.lat': 'latitude',
-        'main.temp': 'temperature',
-        'main.feels_like': 'feels_like',
-        'main.temp_min': 'temp_min',
-        'main.temp_max': 'temp_max',
-        'main.pressure': 'pressure',
-        'main.humidity': 'humidity',
-        'wind.speed': 'wind_speed',
-        'wind.deg': 'wind_direction',
-        'clouds.all': 'cloud_cover',
-        'sys.country': 'country'
-    }, inplace=True)
+    validate_dim_location(dim_location)
+    validate_fact_weather_current(fact_weather_current, dim_location)
 
-    df['location_id'] = city_cfg["location_id"]
+    print(fact_weather_current.date_time.max())
 
-    df['date_time'] = pd.to_datetime(df['date_time'], unit='s', utc=True)
-
-    df = df[
-        [
-            'location_id',
-            'city',
-            'longitude',
-            'latitude',
-            'country',
-            'date_time',
-            'temperature',
-            'feels_like',
-            'temp_min',
-            'temp_max',
-            'pressure',
-            'humidity',
-            'wind_speed',
-            'wind_direction',
-            'cloud_cover',
-            'weather_description'
-        ]
-    ]
-
-    all_current.append(df)
-
-fact_weather_current = pd.concat(all_current, ignore_index=True)
-
-
-dim_location = (
-    fact_weather_current[
-        ['location_id', 'city', 'country', 'latitude', 'longitude']
-    ]
-    .drop_duplicates()
-    .reset_index(drop=True)
-)
-
-validate_dim_location(dim_location)
-
-validate_fact_weather_current(fact_weather_current, dim_location)
-
-fact_weather_current.to_csv(
-    'data/transformed/fact_weather_current.csv',
-    index=False
-)
-
-dim_location.to_csv("data/transformed/dim_location.csv", index=False)
+    fact_weather_current.to_csv('data/transformed/fact_weather_current.csv', index=False)
+    dim_location.to_csv("data/transformed/dim_location.csv", index=False)
--- scripts/fact_weather_daily.py
+++ scripts/fact_weather_daily.py
@@ -1,52 +1,53 @@
-import pandas as pd
-from configs.cities import CITIES
-from scripts.validations import validate_fact_weather_daily
+def run_fact_weather_daily():
+    import pandas as pd
+    from configs.cities import CITIES
+    from scripts.validations import validate_fact_weather_daily
 
-all_daily = []
+    all_daily = []
 
-for city_cfg in CITIES:
-    df = pd.read_csv(
-        city_cfg["historical_file"],
-        skip_blank_lines=True,
-        skiprows=2
-    )
+    for city_cfg in CITIES:
+        df = pd.read_csv(
+            city_cfg["historical_file"],
+            skip_blank_lines=True,
+            skiprows=2
+        )
 
-    df.rename(columns={
-        'time': 'date',
-        'temperature_2m_mean (°C)': 'temp_mean',
-        'temperature_2m_max (°C)': 'temp_max',
-        'temperature_2m_min (°C)': 'temp_min',
-        'precipitation_sum (mm)': 'precipitation',
-        'rain_sum (mm)': 'rain',
-        'snowfall_sum (cm)': 'snowfall',
-        'wind_speed_10m_max (km/h)': 'wind_speed',
-        'wind_direction_10m_dominant (°)': 'wind_direction',
-        'daylight_duration (s)': 'daylight_duration'
-    }, inplace=True)
+        df.rename(columns={
+            'time': 'date',
+            'temperature_2m_mean (°C)': 'temp_mean',
+            'temperature_2m_max (°C)': 'temp_max',
+            'temperature_2m_min (°C)': 'temp_min',
+            'precipitation_sum (mm)': 'precipitation',
+            'rain_sum (mm)': 'rain',
+            'snowfall_sum (cm)': 'snowfall',
+            'wind_speed_10m_max (km/h)': 'wind_speed',
+            'wind_direction_10m_dominant (°)': 'wind_direction',
+            'daylight_duration (s)': 'daylight_duration'
+        }, inplace=True)
 
-    df['date'] = pd.to_datetime(df['date'])
-    df['location_id'] = city_cfg["location_id"]
+        df['date'] = pd.to_datetime(df['date'])
+        df['location_id'] = city_cfg["location_id"]
 
-    df = df[
-    [
-        'location_id',
-        'date',
-        'temp_mean',
-        'temp_max',
-        'temp_min',
-        'precipitation',
-        'rain',
-        'snowfall',
-        'wind_speed',
-        'wind_direction',
-        'daylight_duration'
-    ]
-    ]
+        df = df[
+        [
+            'location_id',
+            'date',
+            'temp_mean',
+            'temp_max',
+            'temp_min',
+            'precipitation',
+            'rain',
+            'snowfall',
+            'wind_speed',
+            'wind_direction',
+            'daylight_duration'
+        ]
+        ]
 
-    all_daily.append(df)
+        all_daily.append(df)
 
-fact_weather_daily = pd.concat(all_daily, ignore_index=True)
+    fact_weather_daily = pd.concat(all_daily, ignore_index=True)
 
-validate_fact_weather_daily(fact_weather_daily)
+    validate_fact_weather_daily(fact_weather_daily)
 
-fact_weather_daily.to_csv('data/transformed/fact_weather_daily.csv', index=False)
+    fact_weather_daily.to_csv('data/transformed/fact_weather_daily.csv', index=False)
